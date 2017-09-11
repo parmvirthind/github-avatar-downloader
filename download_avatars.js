35da7d00bd3b0d1ecfdb2890675111a005da0877
Parmvir <parmvirsthind@gmail.com>
2017-09-11 22:32:09 +0000

    Added requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,14 @@ var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
+var GITHUB_USER = "parmvirthind";
+var GITHUB_TOKEN = "b6b728add7fbdd2f10aa1e5fe451507ac94b8718";
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
 }
-
-
 
 
 
