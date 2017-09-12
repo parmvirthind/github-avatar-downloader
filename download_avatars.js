2d41d0f762b749ab100e0ccbc3796eafecbeec81
Parmvir <parmvirsthind@gmail.com>
2017-09-12 00:18:52 +0000

    Added user agent

--- download_avatars.js
+++ download_avatars.js
@@ -5,10 +5,26 @@ console.log("Welcome to the GitHub Avatar Downloader!");
 var GITHUB_USER = "parmvirthind";
 var GITHUB_TOKEN = "b6b728add7fbdd2f10aa1e5fe451507ac94b8718";
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  console.log(requestURL);
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Avatar Downloader'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    console.log('error:', error);
+    console.log('response:', response.statusCode);
+    console.log('body', body);
+  });
+  // request.get(requestURL)
+  //         .on('response', function (response) {
+  //           console.log('Body:', response);
+  //         })
 }
 
 
